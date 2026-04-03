914472cae9c6fb0c3d42deb851d3eb9a0fd667a4
Leonte Valentin <alexandruvalentinleonte@gmail.com>
2026-04-03 22:31:21 +0300

    Grid + Search integrate cu backend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CatalogPage from "./pages/CatalogPage";
+
+function App() {
+    return (
+        <div>
+            <CatalogPage />
+        </div>
+    );
+}
+
+export default App;

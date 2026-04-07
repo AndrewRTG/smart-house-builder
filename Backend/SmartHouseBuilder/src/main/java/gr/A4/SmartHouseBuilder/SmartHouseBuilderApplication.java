54e61bfae8da38805babea031936fb5fdd17a1e3
Sichim Petru <petrusichim09@gmail.com>
2026-04-07 22:51:09 +0300

    Incearca Vali...

--- Backend/SmartHouseBuilder/src/main/java/gr/A4/SmartHouseBuilder/SmartHouseBuilderApplication.java
+++ Backend/SmartHouseBuilder/src/main/java/gr/A4/SmartHouseBuilder/SmartHouseBuilderApplication.java
@@ -1,7 +1,9 @@
 package gr.A4.SmartHouseBuilder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmartHouseBuilderApplication {
@@ -10,4 +12,12 @@ public class SmartHouseBuilderApplication {
 		SpringApplication.run(SmartHouseBuilderApplication.class, args);
 	}
 
+	/**
+	 * definim manual bean-ul ObjectMapper pentru a rezolva eroarea de injectare
+	 * din LayoutController si pentru a asigura suportul JSON
+	 */
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }

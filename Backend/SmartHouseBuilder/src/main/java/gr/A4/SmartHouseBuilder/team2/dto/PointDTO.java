5850f6fb3d66d5b4130108a089e61c45e755e3d6
Leonte Valentin <alexandruvalentinleonte@gmail.com>
2026-04-07 21:53:47 +0300

    versiune functionala echipa 1 (front + back + docker)

--- Backend/SmartHouseBuilder/src/main/java/gr/A4/SmartHouseBuilder/team2/dto/PointDTO.java
+++
@@ -1,9 +0,0 @@
-package gr.A4.SmartHouseBuilder.team2.dto;
-
-import lombok.Data;
-
-@Data
-public class PointDTO {
-    private Double x;
-    private Double y;
-}
--- Backend/SmartHouseBuilder/src/main/java/gr/A4/SmartHouseBuilder/team2/model/StoredLayout.java
+++
@@ -1,8 +0,0 @@
-package gr.A4.SmartHouseBuilder.team2.model;
-
-import gr.A4.SmartHouseBuilder.team2.dto.SetupBuildDTO;
-
-import java.time.Instant;
-
-public record StoredLayout(Long id, Instant createdAt, SetupBuildDTO data) {
-}

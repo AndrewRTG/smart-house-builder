a84cc434c14045d16aaaddd47a347b67a164c05e
andrei-m-05 <andreimatrescu2005@gmail.com>
2026-05-04 21:25:18 +0300

    StoredLayout.java

--- Backend/SmartHouseBuilder/src/main/java/gr/A4/SmartHouseBuilder/team2/model/StoredLayout.java
+++ Backend/SmartHouseBuilder/src/main/java/gr/A4/SmartHouseBuilder/team2/model/StoredLayout.java
@@ -1,8 +1,12 @@
 package gr.A4.SmartHouseBuilder.team2.model;
 
 import gr.A4.SmartHouseBuilder.team2.dto.SetupBuildDTO;
-
+import lombok.Data;
 import java.time.Instant;
 
-public record StoredLayout(Long id, Instant createdAt, SetupBuildDTO data) {
+@Data
+public class StoredLayout {
+    private Long id;
+    private Instant createdAt;
+    private SetupBuildDTO content;
 }

e57b936139624610d6ad280ed2eae39b478e006a
Leonte Valentin <alexandruvalentinleonte@gmail.com>
2026-05-07 12:22:36 +0300

    test coverage frontend echipa 1

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -13,7 +10,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
-    // Această linie va genera folderul 'coverage' cu raportul de 80%
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],

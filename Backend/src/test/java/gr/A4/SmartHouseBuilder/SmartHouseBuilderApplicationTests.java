2bc5baaee4e7f832641d3809db076556c46c049e
Ailutoaei_Calin <ailutoaei05@yahoo.com>
2026-05-12 05:11:19 +0300

    Profil izolat de test pt backend CI

--- Backend/src/test/java/gr/A4/SmartHouseBuilder/SmartHouseBuilderApplicationTests.java
+++ Backend/src/test/java/gr/A4/SmartHouseBuilder/SmartHouseBuilderApplicationTests.java
@@ -2,8 +2,10 @@ package gr.A4.SmartHouseBuilder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartHouseBuilderApplicationTests {
 
 	@Test
